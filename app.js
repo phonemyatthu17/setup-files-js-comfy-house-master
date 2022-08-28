d991b416dcf26dcab86e459c8f29f9213c27dfde
phonemyatthu17 <94057124+phonemyatthu17@users.noreply.github.com>
2022-08-28 15:59:27 +0630

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,15 @@ class Products {
   async getProducts() {
     try {
       let result = await fetch("products.json");
-      return result;
+      let data = await result.json();
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
     } catch (error) {
       console.log("error");
     }
