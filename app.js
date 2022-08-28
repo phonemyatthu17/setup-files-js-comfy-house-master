2cb5520de4cc86215df4c3694fe9efad26c417f2
phonemyatthu17 <94057124+phonemyatthu17@users.noreply.github.com>
2022-08-28 09:55:00 +0630

    updating

---
+++ app.js
@@ -0,0 +1,10 @@
+// variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCart = document.querySelector(".close-cart");
+const clearCart = document.querySelector(".clear-cart");
+const cartDom = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartContent = document.querySelector(".cart-content");
+const cartItem = document.querySelector(".cart-item");
+const productsDom = document.querySelector(".pr");
