9473325e0ae04e66674dafe5782b9ff223f66cd5
phonemyatthu17 <94057124+phonemyatthu17@users.noreply.github.com>
2022-09-21 15:58:34 +0630

    Update app.js

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ class UI {
             </div>
             <div>
               <i class="fas fa-chevron-up" data-id=${item.id}></i>
-              <p class="${item.amount}">1</p>
+              <p class="${item.amount}">${item.amount}</p>
               <i class="fas fa-chevron-down" data-id=${item.id}></i>
             </div>`;
     cartContent.appendChild(div);
