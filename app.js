e64def2cd19ec418b8cf06097aee10ff8fc89bd4
phonemyatthu17 <94057124+phonemyatthu17@users.noreply.github.com>
2022-09-23 17:39:28 +0630

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ class Products {
 
       let result = await fetch("products.json");
       let data = await result.json();
-      let products = contentful.items;
+      let products = data.items;
       products = products.map((item) => {
         const { title, price } = item.fields;
         const { id } = item.sys;
